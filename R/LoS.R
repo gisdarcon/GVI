eb577c9c82ae1808877e09abc27904ad7512de2e
STBrinkmann <bastibrinkmann94@gmail.com>
2021-03-31 12:25:43 +0200

    First commit

---
+++ R/LoS.R
@@ -0,0 +1,31 @@
+#' @title Line of Sight
+#' @description Computes a single Line of Sight (LoS) using a Digital Surface Model (DSM) matrix.
+#'
+#' @param rc1 integer vector; Row and column of the targets location in the DSM matrix.
+#' @param r0 integer; Specifying the row of the starting location in the DSM matrix.
+#' @param c0 integer; Specifying the column of the starting location in the DSM matrix.
+#' @param dsm_mat integer matrix; Matrix derived from a DSM.
+#' @param observerHeight numeric > 0; Height of the observer (e.g. 1.8)
+#'
+#' @return Cell numbers of all visible cells of the DSM. 
+#' @export
+
+LoS <- function(rc1, r0, c0, dsm_mat, observerHeight) {
+  # End points
+  r1 <- rc1[1]
+  c1 <- rc1[2]
+  
+  # Get XY coordinates of all points in the matrix along a line from r0,c0 to r1,c1
+  pointsZ <- GVI::bresenham(x1 = c0, y1 = r0, x2 = c1, y2 = r1)
+  pointsZmat <- matrix(pointsZ, ncol = 2, byrow = T)
+  
+  # Pull DSM height values from matrix
+  dsm_profile <- dsm_mat[pointsZmat]
+  
+  # Is visible? Current tangent must be greater than max. tangent
+  tangents <- GVI::tangents(x1 = c0, y1 = r0, height0 = observerHeight, xy2 = pointsZ, dsm_profile = dsm_profile)
+  visibile <- GVI::tangents(tangents)
+  
+  # Get visible cell numbers
+  return((pointsZmat[visibile, 2]-1)*nrow(dsm_mat) + pointsZmat[visibile, 1])
+}
---
+++ R/viewshed.R
@@ -0,0 +1,72 @@
+viewshed <- function(sf_start, max_distance, dsm_data, dtm_data, resolution, 
+                     observer_height, plot = FALSE) {
+  
+  # AOI
+  this_aoi <- sf_start %>% 
+    sf::st_buffer(max_distance)
+  
+  # Coordinates of start point
+  x0 <- sf::st_coordinates(sf_start)[1]
+  y0 <- sf::st_coordinates(sf_start)[2]
+  
+  # Observer height
+  height0 <- as.numeric(terra::extract(dtm_data, cbind(x0, y0))) + observer_height
+  
+  # If the resolution parameter differs from the input-DSM resolution,
+  # resample the DSM to the lower resolution.
+  # Also, convert dsm_data_masked to "Raster" object, for faster internal calculation.
+  if ((res(dsm_data)[1] != resolution) & (resolution >= 1)) {
+    dsm_data_masked <- terra::crop(dsm_data, this_aoi) %>% 
+      terra::aggregate(fact = resolution/terra::res(.)) %>% 
+      terra::mask(terra::vect(this_aoi))
+    
+    output <- terra::setValues(dsm_data_masked, 0) %>%
+      terra::mask(dsm_data_masked)
+  } else {
+    dsm_data_masked <- terra::crop(dsm_data, this_aoi) %>% 
+      terra::mask(terra::vect(this_aoi))
+    
+    output <- terra::setValues(dsm_data_masked, 0) %>% 
+      terra::mask(dsm_data_masked)
+  }
+  
+  # Start row/col
+  r0 <- terra::rowFromY(output, y0)
+  c0 <- terra::colFromX(output, x0)
+  
+  # Convert output raster to matrix
+  dsm_mat <- terra::values(dsm_data_masked) %>% 
+    matrix(ncol = terra::ncol(dsm_data_masked))
+  
+  # Calculate boundaries of output raster (boundaries are adjacent to NA values)
+  output_boundaries <- terra::expand(output, resolution*2) %>% 
+    terra::boundaries()
+  
+  # Get rows and columns of boundaries cells and convert to list
+  xy1 <- terra::xyFromCell(output_boundaries, which(terra::values(output_boundaries) == 1))
+  
+  # Row/Col of boundary cells
+  rc1 <- cbind(terra::rowFromY(output, xy1[,2]), 
+               terra::colFromX(output, xy1[,1])) %>% 
+    split(seq(nrow(.)))
+  
+  # Apply lineOfSight function on every point in rc1
+  this_LoS <- lapply(rc1, GVI::LoS, 
+                     r0 = r0, c0 = c0, dsm_mat = dsm_mat, height0 = height0)
+  
+  # Bind list
+  this_LoS <- unlist(this_LoS)
+  
+  # Copy result of lapply to the output raster
+  #output[this_LoS[,1]] <- this_LoS[,2]
+  output[this_LoS] <- 1
+  
+  # Compare DSM with Visibilty
+  if (plot) {
+    par(mfrow=c(1,2))
+    plot(dsm_data_masked); points(x0, y0, col = "red", pch = 20, cex = 2)
+    plot(output, legend = F); points(x0, y0, col = "red", pch = 20, cex = 2)
+    par(mfrow=c(1,1))
+  }
+  return(output)
+}
