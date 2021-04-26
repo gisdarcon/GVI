5a70079b2fb2dafdf1dc9d5b2dcb940201fd6f62
STBrinkmann <bastibrinkmann94@gmail.com>
2021-04-26 19:19:42 +0200

    Description updated

--- R/viewshed.R
+++ R/viewshed.R
@@ -2,11 +2,11 @@
 #' @description Computes the viewshed of a point on a Digital Surface Model map.
 #'
 #' @param sf_start object of class \code{sf} with one point; Starting location
-#' @param max_distance numeric; Buffer distance to calculate the viewshed 
+#' @param max_distance numeric; Buffer distance to calculate the viewshed
 #' @param dsm_data object of class \code{\link[terra]{SpatRaster}}; \code{\link[terra]{SpatRaster}} of the DSM
 #' @param dtm_data object of class \code{\link[terra]{SpatRaster}}; \code{\link[terra]{SpatRaster}} of the DTM
 #' @param observer_height numeric > 0; Height of the observer (e.g. 1.7 meters)
-#' @param resolution optional; numeric > 1; Resolution that the GVI should be aggregated to. 
+#' @param resolution optional; numeric >= 1; Resolution that the GVI should be aggregated to
 #' @param plot optional; Plot DSM and GVI
 #'
 #' @return object of class \code{\link[terra]{SpatRaster}}
@@ -15,6 +15,9 @@
 #' @importFrom magrittr %>%
 #' @importFrom sf st_buffer
 #' @importFrom sf st_coordinates
+#' @importFrom sf st_crs
+#' @importFrom sf st_geometry_type
+#' @importFrom terra crs
 #' @importFrom terra extract
 #' @importFrom terra res
 #' @importFrom terra crop
@@ -30,11 +33,48 @@
 #' @importFrom terra xyFromCell
 #' @importFrom terra plot
 #' @importFrom Rcpp sourceCpp
+#' @importFrom graphics par
+#' @importFrom graphics points
 #' @useDynLib GVI, .registration = TRUE
+
 viewshed <- function(sf_start, dsm_data, dtm_data, 
                      max_distance = 800, observer_height = 1.7, 
                      resolution = NULL, plot = FALSE) {
+  #### 1. Check input ####
+  # sf_start
+  valid_sf_types <- c()
+  if (!is(sf_start, "sf")) {
+    stop("sf_start must be a sf object")
+  } else if (sf::st_crs(sf_start)$units != "m") {
+    stop("sf_start CRS unit needs to be metric")
+  } else if (!as.character(sf::st_geometry_type(sf_start, by_geometry = FALSE)) == "POINT") {
+    stop("sf_start has no valid geometry")
+  } else if (nrow(sf_start) > 1) {
+    stop("The viewshed function can only calculate the viewshed of one point. Please look into the vgvi_from_sf function.")
+  }
   
+  # dsm_data
+  if (!is(dsm_data, "SpatRaster")) {
+    stop("dsm_data needs to be a SpatRaster object!")
+  } else if (sf::st_crs(terra::crs(dsm_data))$epsg != sf::st_crs(sf_start)$epsg) {
+    stop("dsm_data needs to have the same CRS as sf_start")
+  }
+  
+  # dtm_data
+  if (!is(dsm_data, "SpatRaster")) {
+    stop("dtm_data needs to be a SpatRaster object!")
+  } else if (sf::st_crs(terra::crs(dtm_data))$epsg != sf::st_crs(sf_start)$epsg) {
+    stop("dtm_data needs to have the same CRS as sf_start")
+  }
+  
+  # resolution
+  if (is.null(resolution)) {
+    resolution = min(terra::res(dsm_data))
+  } else if (resolution < 1 || resolution < min(terra::res(dsm_data))) {
+    stop("If the resolution differs from dsm_data, it needs to be > 1 and higher than the dsm_data resolution")
+  }
+  
+  #### 2. Prepare Data for viewshed analysis ####
   # AOI
   this_aoi <- sf_start %>% 
     sf::st_buffer(max_distance)
@@ -48,10 +88,6 @@ viewshed <- function(sf_start, dsm_data, dtm_data,
   
   # If the resolution parameter differs from the input-DSM resolution,
   # resample the DSM to the lower resolution.
-  # Also, convert dsm_data_masked to "Raster" object, for faster internal calculation.
-  if (is.null(resolution)) {
-    resolution = min(terra::res(dsm_data))
-  }
   if (resolution == min(terra::res(dsm_data))) {
     dsm_data_masked <- terra::crop(dsm_data, this_aoi) %>% 
       terra::mask(terra::vect(this_aoi))
@@ -69,6 +105,7 @@ viewshed <- function(sf_start, dsm_data, dtm_data,
       terra::mask(dsm_data_masked)
   }
   
+  #### 3. Compute viewshed ####
   # Start row/col
   r0 <- terra::rowFromY(output, y0)
   c0 <- terra::colFromX(output, x0)
@@ -94,12 +131,12 @@ viewshed <- function(sf_start, dsm_data, dtm_data,
   # Copy result of lineOfSight to the output raster
   output[this_LoS] <- 1
   
-  # Compare DSM with Visibility
+  #### 4. Compare DSM with Visibility ####
   if (plot) {
-    par(mfrow=c(1,2))
-    terra::plot(dsm_data_masked, legend = F); points(x0, y0, col = "red", pch = 20, cex = 2)
-    terra::plot(output, legend = F); points(x0, y0, col = "red", pch = 20, cex = 2)
-    par(mfrow=c(1,1))
+    graphics::par(mfrow=c(1,2))
+    terra::plot(dsm_data_masked, legend = F); graphics::points(x0, y0, col = "red", pch = 20, cex = 2)
+    terra::plot(output, legend = F); graphics::points(x0, y0, col = "red", pch = 20, cex = 2)
+    graphics::par(mfrow=c(1,1))
   }
   return(output)
 }
